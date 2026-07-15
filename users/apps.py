e2fadfa2acf3af20efad4d6e2618fbedddb42619
Mitesh-Chandel <chandelmitesh777@gmail.com>
2026-07-15 15:02:58 +0530

    version 1

--- users/apps.py
+++ users/apps.py
@@ -1,5 +1,10 @@
+
 from django.apps import AppConfig
 
 
 class UsersConfig(AppConfig):
     name = 'users'
+
+    def ready(self):
+        # This registers the signals when Django starts up
+        import users.signals
